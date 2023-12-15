c923126fb7f4ec779abc628d9d342187581af6cb
ravik <ravik@RAVIKUMAR>
2023-12-15 20:49:39 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public string DisplayString(string GetString)
+        {
+            string str = "Hi!! I am from C#.NET   " + GetString;
+            return str;
+        }
+    }
+}
